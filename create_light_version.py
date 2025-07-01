680b27a402ea3a5aca50f4004a0719b1744241e7
Nicolas Lemay <8515774+nilem@users.noreply.github.com>
2025-07-01 12:19:11 -0400

    lighter version of json

--- create_light_version.py
+++ create_light_version.py
@@ -1,56 +1,33 @@
 import json
 
-# Properties to remove
-properties_to_remove = [
-    "POTEAU_ID_POT", "PANNEAU_ID_PAN", "PANNEAU_ID_RPA", "TOPONYME_PAN",
-    "DESCRIPTION_CAT", "POTEAU_VERSION_POT", "DATE_CONCEPTION_POT",
-    "PAS_SUR_RUE", "DESCRIPTION_RTP", "X", "Y", "NOM_ARROND",
-    "Longitude", "Latitude", "DESCRIPTION_REP", "POSITION_POP", "CODE_RPA", "FLECHE_PAN"
-]
-
-# Properties to rename
-properties_to_rename = {
-    "DESCRIPTION_RPA": "D"
-}
-
 # Read the original GeoJSON file
 with open('assets/signalisation_stationnement_full.json', 'r') as f:
     data = json.load(f)
 
+output_array = []
+
 # Filter features and modify properties
 if 'features' in data:
     # Filter out features where DESCRIPTION_REP is "Enlevé"
+    # and features where DESCRIPTION_RPA contains "EN TOUT TEMPS"
     filtered_features = [
         feature for feature in data['features']
-        if feature.get('properties', {}).get('DESCRIPTION_REP') != 'Enlevé'
+        if (feature.get('properties', {}).get('DESCRIPTION_REP') != 'Enlevé' and
+            "EN TOUT TEMPS" not in feature.get('properties', {}).get('DESCRIPTION_RPA', ''))
     ]
     
     for feature in filtered_features:
-        if 'properties' in feature:
+        if 'properties' in feature and 'geometry' in feature:
             properties = feature['properties']
+            geometry = feature['geometry']
             
-            # Remove specified properties
-            for prop in properties_to_remove:
-                if prop in properties:
-                    del properties[prop]
-            
-            # Rename specified properties
-            props_to_add = {}
-            props_to_delete = []
-            for old_name, new_name in properties_to_rename.items():
-                if old_name in properties:
-                    props_to_add[new_name] = properties[old_name]
-                    props_to_delete.append(old_name)
-            
-            for prop in props_to_delete:
-                del properties[prop]
-            
-            properties.update(props_to_add)
-    
-    data['features'] = filtered_features
+            if 'DESCRIPTION_RPA' in properties and geometry and 'coordinates' in geometry:
+                # GeoJSON coordinates are [longitude, latitude]
+                longitude, latitude = geometry['coordinates']
+                output_array.append([latitude, longitude, properties['DESCRIPTION_RPA']])
 
 # Write the modified data to a new file
 with open('assets/signalisation_stationnement_light.json', 'w') as f:
-    json.dump(data, f)
+    json.dump(output_array, f)
 
 print("Lighter version of the GeoJSON file created as assets/signalisation_stationnement_light.json")
