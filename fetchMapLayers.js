8e9e2069d1533b417adf9ed8085e0d784ce329bc
Nicolas Lemay <8515774+nilem@users.noreply.github.com>
2025-08-11 22:24:11 -0400

    Revert "test"
    This reverts commit a7b5c4e8cdad9357f95143e4d98987cd03e12413.

--- fetchMapLayers.js
+++ fetchMapLayers.js
@@ -1,5 +1,5 @@
-// Import de la configuration (fonctionne en dev ET production)
-import config from './config.js';
+// Import de la configuration locale (non versionnée)
+import config from './config.local.js';
 
 /**
  * Récupère un nouveau jeton d'accès depuis le service d'identité.
