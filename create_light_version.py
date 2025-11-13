93096edf6e4c832fad0c21f91dcb69634dc076e9
Nicolas Lemay <8515774+nilem@users.noreply.github.com>
2025-11-13 14:55:15 -0500

    remove signalisation that we do not need

--- create_light_version.py
+++ create_light_version.py
@@ -6,17 +6,49 @@ with open('assets/signalisation_stationnement_full.json', 'r') as f:
 
 output_array = []
 
+def should_exclude_sign(description):
+    """
+    Retourne True si la signalisation doit être exclue.
+    On exclut les panneaux sans notion de temps (autobus, taxis, livraison, etc.)
+    """
+    if not description:
+        return True
+    
+    desc_upper = description.upper()
+    
+    # Exclusions: panneaux sans notion de temps
+    exclusion_keywords = [
+        'AUTOBUS',
+        'TAXI',
+        'ARRÊT INTERDIT',
+        'ARRET INTERDIT',
+        'DEBARCADERE',
+        'DÉBARCADÈRE',
+        'LIVRAISON SEULEMENT',
+        'RESERVE AUTOBUS',
+        'ZONE D\'AUTOBUS',
+        'DEBAR. AUTOBUS',
+        'PANONCEAU TAXIS',
+        'EN TOUT TEMPS',
+        'RESERVE'
+    ]
+    
+    for keyword in exclusion_keywords:
+        if keyword in desc_upper:
+            return True
+    
+    return False
+
 # Filter features and modify properties
 if 'features' in data:
     # Filter out features where:
     # - DESCRIPTION_REP is "Enlevé"
-    # - DESCRIPTION_RPA contains "EN TOUT TEMPS"
     # - DESCRIPTION_RPA starts with "\P RESERVE" (reserved parking, not useful for us)
+    # - DESCRIPTION_RPA doesn't have time-based restrictions (autobus, taxis, etc.)
     filtered_features = [
         feature for feature in data['features']
         if (feature.get('properties', {}).get('DESCRIPTION_REP') != 'Enlevé' and
-            "EN TOUT TEMPS" not in feature.get('properties', {}).get('DESCRIPTION_RPA', '') and
-            not feature.get('properties', {}).get('DESCRIPTION_RPA', '').startswith('\\P RESERVE'))
+            not should_exclude_sign(feature.get('properties', {}).get('DESCRIPTION_RPA', '')))
     ]
     
     for feature in filtered_features:
